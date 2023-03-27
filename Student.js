207e20261ae15421215c59527d1c75c99ce7dd7d
Faisal Raheem Baloch <111741362+faisalraheem360@users.noreply.github.com>
2023-03-27 16:26:07 +0500

    Add files via upload

---
+++ Student.js
@@ -0,0 +1,119 @@
+﻿
+$('#tbllist').ready(function () {
+    table = $('#tbllist').DataTable({
+        "searching": false,
+        "ajax": {
+            "type": "GET",
+            "url": "https://localhost:7059/api/Student",
+            "dataSrc": function (json) {
+                //Make your callback here.
+               // alert("Done!");
+               return json.data;
+            }
+        },
+        "columns": [
+            { "data": "stuId" },
+            { "data": "name" },
+            { "data": "fatherName" },
+            { "data": "age" },
+            { "data": "standard" },
+            {
+                "data":"stuId",
+                mRender: function (data, type, row) {
+                    return '<button type = "button" data-target="#AddStudent"class="btn btn-danger Delete_Btn" data-id="' + data + '">DELETE</button > <button type = "button" class="btn btn-primary Edit_Btn" data-id="' + data + '"onclick="getForm(' + data + ')">Edit</button >';
+                }
+            },          
+        ]
+    });
+});
+//Delete Method with ajax
+$(document).on('click', '.Delete_Btn', function () {
+    let stuId = $(this).data('id');
+    console.log(stuId);
+    if (confirm('Are you Sure to delete?')) {
+        DeleteStudent(stuId);
+    } else {
+        alert("not deleted");
+        table.ajax.reload();
+    }
+});
+
+function DeleteStudent(data) {
+    $.ajax({
+        url: 'https://localhost:7059/api/Student/DeleteStudent/' + data,
+        dataType: "json",
+        success: function (data) {
+            table.ajax.reload();
+        },
+        error: function () {
+        }
+    });
+}
+
+$(document).on('click', '.Edit_Btn', function () {
+    $('#AddStudent').modal('show'); 
+});
+
+/*Get Form*/
+function getForm(id = 0) {
+            $.ajax({
+                url: 'https://localhost:7059/api/Student/GetStudentByID/' + id,
+                data: $("form").serialize(),
+                success: function (data) {
+                    console.log(data)
+                    $('#name').val(data.name)
+                    $('#age').val(data.age)
+                    $('#stuId').val(data.stuId)
+                    $('#fname').val(data.fatherName)
+                    $('#standard').val(data.standard)
+                }
+            });
+        
+}
+
+$('#savebtn').on('click', function () {
+    //debugger
+    SaveStudent();
+    // refreshPage();
+    console.log($("form").serialize())
+    var formData = {
+        StuId: $("#stuId").val(),
+        Name: $("#name").val(),
+        FatherName: $("#fname").val(),
+        Age: $("#age").val(),
+        Standard: $("#standard").val(),
+
+    };
+    console.log(JSON.stringify(formData));
+    $.ajax({
+        url: 'https://localhost:7059/api/Student',
+        headers: {
+            'Content-Type': 'application/json',
+            /*'charset': 'utf-8'*/
+        },
+        type: "Post",
+        data: JSON.stringify(formData),
+        success: function (data) {
+            if (StuId == null || StuId == 0) {
+                $('#AddStudent').modal('hide');
+            }
+            table.ajax.reload();
+            
+
+        }
+    });
+})
+
+$('#addbtn').on('click', function () {
+    $('#AddStudent').modal('hide');
+})
+function SaveStudent() {
+
+    $('#AddStudent').modal('hide'); 
+    /*setInterval('refreshPage()');*/
+ 
+}
+ 
+function refreshPage() {
+    location.reload(true);
+}
---
+++ StudentController.cs
@@ -0,0 +1,68 @@
+﻿using CleanARCHiLMS_Application;
+using CleanARCHiLMS_Domain;
+using CleanARCHiLMS_Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanARCHiLMS_Api.ApiContollers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentServices _studentServices;
+        private readonly StudentDBContext _context;
+
+        public StudentController(IStudentServices studentServices,StudentDBContext context)
+        {
+            _studentServices = studentServices;
+            _context = context;
+        }
+        // GET: api/<StudentController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var students = _studentServices.GetAllStudents();
+            return Ok(new { data = students });
+        }
+        [HttpGet("GetStudentByID/{ID}")]
+        public Student GetStudentByID(int ID)
+        {
+            var students = _studentServices.GetStudentByID(ID);
+            return students;
+        }
+        [HttpPost]
+        public ActionResult<Student> postStudents(Student student)
+        {
+            if(student.StuId!=0)
+            {
+               var us= _studentServices.UpdateStudent(student);
+                return Ok(us);   
+            }
+            else
+            {
+                var students = _studentServices.CreateStudent(student);
+                return Ok(new { data = students });
+            }
+         
+        } 
+        // DELETE: api/Students/5
+     //   [HttpDelete("{id}")]
+        [HttpGet("DeleteStudent/{id}")]
+        public ActionResult<Student> DeleteStudent(int id)
+        {
+            var student = _context.students.Find(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _context.students.Remove(student);
+            _context.SaveChanges();
+
+            return student;
+        }
+    }
+}
